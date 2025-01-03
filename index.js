2114221eea10553f11f25ef366c6ff756e15f0d9
Ajsal sparky <130032226+Axl-Sparky@users.noreply.github.com>
2025-01-03 20:42:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require('path');
 // const getDeviceName = require('./hack'); // Import the userAgent module
 
 // Get the bot token from environment variables
-const token = "7388778092:AAEo4Nhm5LM-cc3fvxPCb6ifyNzH1KUz9KEn";
+const token = "7388778092:AAEo4Nhm5LM-cc3fvxPCb6ifyNzH1KUz9KE";
 
 // Create a bot instance
 const bot = new TelegramBot(token, { polling: true });
